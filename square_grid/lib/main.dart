3e53838f974d8b063826529cdcd6771c12a82f83
Aileen Morgan <amorgan101010@protonmail.com>
2022-07-19 16:58:52 -0400

    Reworked grid to have color and padding

--- square_grid/lib/main.dart
+++ square_grid/lib/main.dart
@@ -52,28 +52,31 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
           child: GridView.count(
-        crossAxisCount: 4,
-        children: List.generate(16, (index) {
-          return Center(child: Text((index + 1).toString()));
-        }),
-      )),
-      // This trailing comma makes auto-formatting nicer for build methods.
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(35),
+              crossAxisCount: 4,
+              children: List.generate(
+                16,
+                (index) {
+                  // TODO: Add an outline
+                  // TODO: Make the fill color configurable
+                  return Card(
+                    child: Container(
+                      color: Colors.blue,
+                      padding: const EdgeInsets.all(5),
+                      child: Center(
+                        child: Text((index + 1).toString()),
+                      ),
+                    ),
+                  );
+                },
+              ))),
     );
   }
 }
