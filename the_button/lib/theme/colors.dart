b363cf5f10e3da5d1e38cb0c91d9b233f12bcca1
Aileen Morgan <amorgan101010@protonmail.com>
2022-07-30 10:32:46 -0400

    Reverted outdated changes...

---
+++ the_button/lib/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color purple = Color.fromARGB(255, 130, 51, 144);
+  static const Color lightPurple = Color.fromARGB(255, 180, 114, 192);
+}
---
+++ the_button/lib/theme/custom_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class CustomTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+        primaryColor: CustomColors.purple,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Montserrat',
+        buttonTheme: ButtonThemeData(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+          buttonColor: CustomColors.lightPurple,
+        ));
+  }
+}
